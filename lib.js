943af00189393d2886efc784177c51c7bec5477f
yryun00 <youngro317@gmail.com>
2023-11-01 18:39:28 +0900

    test later

--- lib.js
+++ lib.js
@@ -3,12 +3,23 @@ function avg(numbers) {
     return s / numbers.length;
 }
 function prime(num) {
-    // TODO
-    return false;
+    // input num is a prime
+    for(let a=2; num>a; a++){
+        if(num%a == 0){
+            return false;
+        }
+    }
+    return true;
 }
 function fact(num) {
+    // num is up to 15
+    if(num>15) return false;
     // TODO
-    return -1;
+    let result = 1;
+    for(let a=2; a<=num; a++){
+        result *= a;
+    }
+    return result;
 }
 module.exports = {
     avg,
--- lib.test.js
+++ lib.test.js
@@ -1,10 +1,29 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
 
+// test average number
 test("avg([3, 5, 7]) should be 5", () => {
     expect(lib.avg([3, 5, 7])).toBe(5);
 });
-
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
+});
+
+// test prime number
+test("prime([3, 4]) should be true", () => {
+    expect(lib.prime([3, 4])).toBe(true);
+});
+test("prime(8) should be false", () => {
+    expect(lib.prime(8)).toBe(false);
+});
+
+// test calculate factorial number
+test("factorial([3, 5]) should be 6", () => {
+    expect(lib.fact(3)).toBe(6);
+});
+test("factorial(5) should be 120", () => {
+    expect(lib.fact(5)).toBe(120);
+});
+test("factorial(20) should be false", () => {
+    expect(lib.fact(20)).toBe(false);
 });
